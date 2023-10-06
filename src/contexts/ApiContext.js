7de27b14fba34de0cf456a275b9410690db410a6
Katie Lock <katielock92@gmail.com>
2023-10-07 10:36:18 +1100

    feat: fetch data from API and display it (#5)
    * all files up to class living coding, not working as expected
    
    * feat: react bootstrap installed and card arrays rendered

--- src/contexts/ApiContext.js
+++ src/contexts/ApiContext.js
@@ -1,15 +1,27 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react";
 
 
-export const ApiContext = createContext(null)
+export const ApiContext = createContext(null);
 
-export default function ApiProvider(children) {
+export default function ApiProvider ({children}){
 
-    const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/");
+	const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/v2/");
 
-    return (
-        <ApiContext.Provider value={{apiUrl: apiUrl, setApiUrl: setApiUrl}}>
-            {children}
-        </ApiContext.Provider>
-    );
+	// useEffect(() => {
+	// 	setApiUrl("https://api.pokemontcg.io/");
+	// }, []);
+
+	return (
+		<ApiContext.Provider value={
+			{
+				api: apiUrl, 
+				setApi: setApiUrl
+			}
+		}>
+
+			{children}
+
+		</ApiContext.Provider>
+	)
+
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import ApiProvider from "./contexts/UserCardDataContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ApiProvider from './contexts/ApiContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CardSearchByName from './pages/CardSearchByName';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApiProvider>
-      <App />
+      
+      <BrowserRouter>
+        <Routes>
+          {/* Homepage route  */}
+          <Route path="/" element={<App />}/>
+          {/* API fetch route */}
+          <Route path="/card/search/:pokemonName" element={<CardSearchByName />} />
+
+          
+        </Routes>
+      
+      </BrowserRouter>
+      
     </ApiProvider>
+    
   </React.StrictMode>
 );
 
