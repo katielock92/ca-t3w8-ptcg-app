2f8039ea99e55fd6a680b74150f25f6648bc9d89
katielock92 <katielock92@gmail.com>
2023-10-07 12:06:08 +1100

    feat: card gallery styled

--- src/pages/CardSearchByName.jsx
+++ src/pages/CardSearchByName.jsx
@@ -1,7 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react"
 import { ApiContext } from "../contexts/ApiContext";
 import { useParams } from "react-router-dom";
 import PokemonCard from "../components/PokemonCard";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function CardSearchByName() {
 
@@ -39,6 +42,7 @@ export default function CardSearchByName() {
 
 		apiRequest();
 
+
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
@@ -49,13 +53,24 @@ export default function CardSearchByName() {
 			<div>
 				<h1>{searchResults[0].name} - {searchResults[0].id}</h1>
 
-				{searchResults.map(result => {
-					return <PokemonCard key={result.id} 
-					cardTitle={result.name} 
-					imageUrl={result.images.small} 
-					cardDescription={result.flavorText} 
-					/>
-				})}
+
+				{/* container here with rows and columns */}
+				{/* put card in columns and let them responsively organise themselves  */}
+				<Container fluid style={{padding: "5%"}}>
+					<Row style={{display: 'flex', flexWrap:'wrap'}}>
+						{searchResults.map(result => {
+							return <Col className="my-2" md lg="2">
+							<PokemonCard key={result.id} 
+								cardTitle={result.name} 
+								imageUrl={result.images.small} 
+								cardDescription={result.flavorText} 
+							/>
+							</Col>
+						})}
+					</Row>
+				</Container>
+
+
 
 
 			</div>
